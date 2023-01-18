0affea35d4e01f7b480a4edfa2c292428d31d5cc
AngeloTripodi <tripodi.angelo@gmail.com>
2023-01-18 19:27:10 +0100

    Add header

--- src/App.vue
+++ src/App.vue
@@ -25,5 +25,5 @@ export default {
 <style lang="scss">
 @use './styles/general.scss' as *;
 @use './styles/partials/variables' as *;
-@use 'bootstrap/scss/bootstrap.scss' as *;
+// @use 'bootstrap/scss/bootstrap.scss' as *;
 </style>
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -3,7 +3,39 @@
 export default {
     data() {
         return {
+            navList: [
+                {
+                    title: 'Home',
+                    isActive: true,
+                    chevron: true,
+                },
+                {
+                    title: 'Services',
+                    isActive: false,
+                    chevron: true,
+                },
+                {
+                    title: 'Why Us',
+                    isActive: false,
+                    chevron: false,
+                },
+                {
+                    title: 'Case Studies',
+                    isActive: false,
+                    chevron: false,
+                },
+                {
+                    title: 'About',
+                    isActive: false,
+                    chevron: false,
+                },
+                {
+                    title: 'Blog',
+                    isActive: false,
+                    chevron: false,
+                },
 
+            ]
         };
     },
     methods: {
@@ -14,11 +46,65 @@ export default {
 </script>
 
 <template>
+    <header>
+        <div class="my_container">
+            <div class="logoSection">
+                <img src="../assets/images/avada-marketing-logo-2x.png" alt="Avada Marketing Logo">
+            </div>
+            <nav class="navSection">
+                <ul>
+                    <li v-for="navElement in navList" :class="(navElement.isActive) ? 'active' : ''">
+                        {{ navElement.title }}
+                    </li>
+                </ul>
+            </nav>
+            <div class="phoneCta">
 
-    <h1>header</h1>
-
+            </div>
+        </div>
+    </header>
 </template>
 
 <style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
 
+.active {
+    color: $main-orange;
+    border-bottom: 2px solid $main-orange;
+}
+
+
+
+.my_container {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+
+
+    .logoSection img {
+        width: 240px;
+    }
+
+    ul {
+        list-style-type: none;
+        display: flex;
+        align-items: center;
+    }
+
+    li {
+        padding: 0 .5rem;
+        margin: 0 .5rem;
+        padding-bottom: .5rem;
+    }
+
+    li:hover {
+        color: $main-orange;
+    }
+}
 </style>
+
+
+
+
